40c226d752f4feebd50fc03b62de67caddfe1cab
sd918207458 <151949617+sd918207458@users.noreply.github.com>
2024-02-18 19:48:03 +0800

    Update uiContainer.js

--- AR模板/Script/uiContainer.js
+++ AR模板/Script/uiContainer.js
@@ -1,16 +1,17 @@
-    <!-- JavaScript代碼 -->
-    <script>        
-            // 獲取關閉按鈕元素
-            let closeButton = document.getElementById('closeButton');
-        
-            // 獲取uiContainer元素
-            let uiContainer = document.getElementById('uiContainer');
-        
-            // 監聽關閉按鈕的點擊事件
-            closeButton.addEventListener('click', function () {
-                // 隱藏uiContainer和closeButton
-                uiContainer.style.display = 'none';
-                closeButton.style.display = 'none';
-            });
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        // 獲取關閉按鈕元素
+        let closeButton = document.getElementById('closeButton');
 
-    </script>
+        // 獲取uiContainer元素
+        let uiContainer = document.getElementById('uiContainer');
+
+        // 監聽關閉按鈕的點擊事件
+        closeButton.addEventListener('click', function() {
+            // 隱藏uiContainer和closeButton
+            uiContainer.style.display = 'none';
+            closeButton.style.display = 'none';
+        });
+    });
+</script>
+
