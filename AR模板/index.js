583577f901d4b651a56a3c9c2d265b0c2524aacd
sd918207458 <151949617+sd918207458@users.noreply.github.com>
2024-02-18 11:10:23 +0800

    Update index.js

--- AR模板/index.js
+++ AR模板/index.js
@@ -1,1 +1,17 @@
+    <!-- JavaScript代码 -->
+    <script>
 
+        // 获取关闭按钮元素
+        let closeButton = document.getElementById('closeButton');
+
+        // 获取uiContainer元素
+        let uiContainer = document.getElementById('uiContainer');
+
+        // 监听关闭按钮的点击事件
+        closeButton.addEventListener('click', function () {
+            // 隐藏uiContainer和closeButton
+            uiContainer.style.display = 'none';
+            closeButton.style.display = 'none';
+        });
+
+    </script>
