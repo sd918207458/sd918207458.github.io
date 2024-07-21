818169fba6b86808ccaf75764eb3a5dbd10c9673
hoxton <sd918207458@gmail.com>
2024-07-21 18:37:17 +0800

    調整跳關樣式

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -253,23 +253,25 @@ const Login = () => {
             visible={isModalVisible}
             onOk={handleModalOk}
             onCancel={handleModalCancel}
+            footer={null}
           >
-            <Button onClick={() => navigateToGame(1)}>Game 1</Button>
-            <Button onClick={() => navigate('/', { state: { dialogIndex: 5 } })}>Game 1 後</Button>
-            <Button onClick={() => navigateToGame(2)}>Game 2</Button>
-            <Button onClick={() => navigate('/', { state: { dialogIndex: 10 } })}>Game 2 後</Button>
-            <Button onClick={() => navigateToGame(3)}>Game 3</Button>
-            <Button onClick={() => navigate('/', { state: { dialogIndex: 22 } })}>Game 3 後</Button>
-            <Button onClick={() => navigateToGame(4)}>Game 4</Button>
-            <Button onClick={() => navigate('/', { state: { dialogIndex: 37 } })}>Game 4 後</Button>
-            <Button onClick={() => navigateToGame(5)}>Game 5</Button>
-            <Button onClick={() => navigate('/', { state: { dialogIndex: 38 } })}>Game 5 後</Button>
-            <Button onClick={() => navigateToGame(6)}>Game 6</Button>
-            <Button onClick={() => navigate('/', { state: { dialogIndex: 68 } })}>Game 6 後</Button>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+              <Button onClick={() => navigateToGame(1)}>Game 1</Button>
+              <Button onClick={() => navigate('/', { state: { dialogIndex: 5 } })}>Game 1 後</Button>
+              <Button onClick={() => navigateToGame(2)}>Game 2</Button>
+              <Button onClick={() => navigate('/', { state: { dialogIndex: 10 } })}>Game 2 後</Button>
+              <Button onClick={() => navigateToGame(3)}>Game 3</Button>
+              <Button onClick={() => navigate('/', { state: { dialogIndex: 22 } })}>Game 3 後</Button>
+              <Button onClick={() => navigateToGame(4)}>Game 4</Button>
+              <Button onClick={() => navigate('/', { state: { dialogIndex: 37 } })}>Game 4 後</Button>
+              <Button onClick={() => navigateToGame(5)}>Game 5</Button>
+              <Button onClick={() => navigate('/', { state: { dialogIndex: 38 } })}>Game 5 後</Button>
+              <Button onClick={() => navigateToGame(6)}>Game 6</Button>
+              <Button onClick={() => navigate('/', { state: { dialogIndex: 68 } })}>Game 6 後</Button>
+            </div>
           </Modal>
         </>
-      )
-      }
+      )}
       {
         dialogVisible && (
           <Dialog
